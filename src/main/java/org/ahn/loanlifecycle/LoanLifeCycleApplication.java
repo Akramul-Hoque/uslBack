35d4451b1e3dc6e36464a301e4427abb1ebe3498
nahid <akramulhoque105@gmail.com>
2026-02-01 12:35:58 +0600

    first commit

---
+++ src/main/java/org/ahn/loanlifecycle/LoanLifeCycleApplication.java
@@ -0,0 +1,13 @@
+package org.ahn.loanlifecycle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanLifeCycleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanLifeCycleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/ahn/loanlifecycle/LoanLifeCycleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ahn.loanlifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanLifeCycleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
